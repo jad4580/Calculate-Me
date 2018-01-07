9db5ff5e961a2a598e0807871fd7fd0f13cfe5b7
jad4580 <jdoaner98@yahoo.com>
2018-01-06 22:36:29 -0500

    Initial Commit

---
+++ CalculateMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculateMe
+//
+//  Created by Justin Doan on 1/6/18.
+//  Copyright © 2018 Justin Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
